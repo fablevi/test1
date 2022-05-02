438b90eb0196b5b51dffbb27c11bd7b6462b3537
hallgato <hallgato@mail.org>
2022-05-02 18:30:46 +0200

    normal parse

--- src/test1/mainclass.java
+++ src/test1/mainclass.java
@@ -4,13 +4,13 @@ public class mainclass {
 
 	public static void main(String[] args) {
 		//localgitproba
-		int osszeg = osszead (13,5);
+		int osszeg = osszead ("13","5");
 		System.out.print(osszeg);
 	}
 
 	private static int osszead(String a, String b) {
 		// TODO Auto-generated method stub
-		return (int) a+b;
+		return Integer.parseInt(a+b);
 	}
 
 }
