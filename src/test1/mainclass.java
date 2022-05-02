1da4603f7e7114b066b7d79033ef7a82372e0e4b
hallgato <hallgato@mail.org>
2022-05-02 17:49:32 +0200

    osszead

--- src/test1/mainclass.java
+++ src/test1/mainclass.java
@@ -3,8 +3,12 @@ package test1;
 public class mainclass {
 
 	public static void main(String[] args) {
+		int osszeg = osszead (3,5);
+	}
+
+	private static int osszead(int a, int b) {
 		// TODO Auto-generated method stub
-		System.out.print("he");
+		return a+b;
 	}
 
 }
